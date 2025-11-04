194f5bd89e25cbc8a6f0d1672c3ffd51b8f958e4
Maghizhan-05 <maghizhankamaraj@gmail.com>
2025-11-04 09:52:36 +0530

    Functionality: Refactor signup page with form validation and loading state; add AuthImagePattern component for visual enhancement

--- chat_app/backend/src/index.js
+++ chat_app/backend/src/index.js
@@ -9,18 +9,19 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5001;
-
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
